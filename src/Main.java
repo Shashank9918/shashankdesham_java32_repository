eb8e4a9d1e1f67be351de1bec45e322ecefa52f8
Shashank9918 <sreddy9927@gmail.com>
2023-06-04 23:44:20 -0500

    Assignment1 Day2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Student student1 = new Student();
+        student1.setName("Shashank");
+        student1.getName();
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name;
+    String getName() {
+        System.out.println(this.name);
+        return this.name;
+
+    }
+    void setName(String name) {
+        this.name=name;
+    }
+}
