07ef5ef28a7db5ac32c0b961baa6729e543fe509
Shashank9918 <sreddy9927@gmail.com>
2023-06-09 11:47:32 -0500

    day7

---
+++ src/day8/Vowels.java
@@ -0,0 +1,19 @@
+package day8;
+
+public class Vowels {
+    public static void main(String[] args){
+        String str = "hello how are you";
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' ||ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u'){
+                count++;
+
+            }
+
+        }
+        System.out.println(count);
+    }
+
+}
+
