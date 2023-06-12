7d32f252ea9698531725f08485a9fee84f605cb9
Shashank9918 <sreddy9927@gmail.com>
2023-06-12 15:07:49 -0500

    day8

---
+++ src/day8/Anagram.java
@@ -0,0 +1,26 @@
+package day8;
+import java.util.Arrays;
+
+public class Anagram {
+
+
+
+        public static void main(String[] args){
+            String s1 = "iceman";
+            String s2 = "cinemaa";
+            boolean result;
+            if(s1.length()!=s2.length()){
+                System.out.println(false);
+            }else {
+                char[] arr_s1 = s1.toCharArray();
+                char[] arr_s2 = s2.toCharArray();
+                Arrays.sort(arr_s2);
+                Arrays.sort(arr_s1);
+                result = Arrays.equals(arr_s2, arr_s1);
+                System.out.println(result);
+            }
+        }
+
+
+    }
+
